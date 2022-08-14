3ea5f4ea0296f5aa3b5c07e32ac60382c4126b78
mojave2 <0109chenchen@gmail.com>
2022-08-14 21:18:01 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod vec;
+
+use std::io::{stderr, Write};
+use vec::{Vec3, Color};
+
+fn main() {
+    const IMAGE_WIDTH: u64 = 256;
+    const IMAGE_HEIGHT: u64 = 256;
+
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("\rScanlines remaining: {:3}", j+1);
+        stderr().flush().unwrap();
+        for i in 0..IMAGE_WIDTH {
+            let pixel_color = Color::new(
+                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
+                (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
+                0.25,
+            );
+            println!("{}", pixel_color.format_color());
+        }
+    }
+    eprintln!("Done.");
+}
